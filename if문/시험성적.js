54117f63038fd5bbff1a3374eeec03c2c9dbece0
shinhu <shinhu91@gmail.com>
2022-01-12 21:37:32 +0900

    시험성적

---
+++ if문/시험성적.js
@@ -0,0 +1,18 @@
+"use strict";
+const fs = require("fs");
+
+// let input = fs.readFileSync("/dev/stdin").toString();
+let input = fs.readFileSync("example.txt").toString();
+
+const score = Number(input);
+if (90 <= score && score <= 100) {
+    console.log("A");
+} else if (80 <= score && score <= 89) {
+    console.log("B");
+} else if (70 <= score && score <= 79) {
+    console.log("C");
+} else if (60 <= score && score <= 69) {
+    console.log("D");
+} else {
+    console.log("F");
+}
